d234369123300a36f7fb84140c16afb12677749d
michalgdak <michal.gdak@gmail.com>
2018-04-26 03:55:02 +0200

    Added data split into DTV

--- processData.py
+++ processData.py
@@ -17,8 +17,8 @@ from pprint import pprint
 import MySQLdb
 
 SQL_CMD_INSERT = u'INSERT INTO reviews (`review`,`source`,`rating`,`source_id`, `pos_neg`) values (%s, %s, %s, %s, %s)'
-SQL_CMD_ALL_NEG = u'select review_id from reviews where pos_neg = 0'
-SQL_CMD_UPDATE_DTV_CLASS = u'update imdb.review set dtv_classification = %s where review_id = %s'
+SQL_CMD_SELECT_ALL = u'select review_id from reviews'
+SQL_CMD_UPDATE_DTV_CLASS = u'update imdb.reviews set dtv_classification = %s where review_id = %s'
 
 TRAIN_SPLIT = 0.5
 TEST_SPLIT = 0.25
@@ -71,7 +71,7 @@ def importDataFromFilesToDB(args):
 
 def updateTDVClassification(dbc, dataSet, setType):
     for revId in dataSet:
-        dbc.execute(SQL_CMD_UPDATE_DTV_CLASS, (revId, setType))
+        dbc.execute(SQL_CMD_UPDATE_DTV_CLASS, (setType, revId))
 
 
 def splitData(dataToSplit, dbc, startIdx, endIdx, setType):
@@ -81,46 +81,48 @@ def splitData(dataToSplit, dbc, startIdx, endIdx, setType):
 
 def setDataSetSize(dataToSplit):
     numOfRecords = len(dataToSplit)
-    trainSetSize = numOfRecords * TRAIN_SPLIT
-    testSetSize = numOfRecords * TEST_SPLIT
-    valSetSize = numOfRecords * VAL_SPLIT
+    trainSetSize = int(numOfRecords * TRAIN_SPLIT)
+    testSetSize = int(numOfRecords * TEST_SPLIT)
+    valSetSize = int(numOfRecords * VAL_SPLIT)
     return trainSetSize, testSetSize, valSetSize
 
 
 def extractIdsFromDBToArray(args, sql):
     dataToSplit = []
     dbc, db = dbConnectionInit(args)
-    dbc.execute(SQL_CMD_ALL_NEG)
+    dbc.execute(SQL_CMD_SELECT_ALL)
     for review_id in dbc:
         dataToSplit.append(review_id[0])
     
     random.shuffle(dataToSplit)
     return dataToSplit, dbc, db
 
+
+def splitDataSets(args):
+    dataToSplit, dbc, db = extractIdsFromDBToArray(args, SQL_CMD_SELECT_ALL)
+    trainSetSize, testSetSize, valSetSize = setDataSetSize(dataToSplit)
+    splitData(dataToSplit, dbc, 0, trainSetSize - 1, 1)
+    splitData(dataToSplit, dbc, trainSetSize, trainSetSize + testSetSize - 1, 2)
+    splitData(dataToSplit, dbc, trainSetSize + testSetSize, trainSetSize + testSetSize + valSetSize - 1, 3)
+    db.commit()
+    db.close()
+
 def main(args):
+    import pydevd;pydevd.settrace();
     pprint(args)
     
     if args.mode == 'Import':
         importDataFromFilesToDB(args)
-    elif args.mode == 'TDV_Classification':
-        dataToSplit, dbc, db = extractIdsFromDBToArray(args, SQL_CMD_ALL_NEG)
-        
-        trainSetSize, testSetSize, valSetSize = setDataSetSize(dataToSplit)
-        
-        splitData(dataToSplit, dbc, 0, trainSetSize -1, 1)
-        splitData(dataToSplit, dbc, trainSetSize, trainSetSize + testSetSize -1, 2)
-        splitData(dataToSplit, dbc, trainSetSize + testSetSize, trainSetSize + testSetSize + valSetSize -1, 3)
-                
-        db.commit()
-        db.close()
+    elif args.mode == 'TDV':
+        splitDataSets(args)
 
 def parse_arguments(argv):
     
     parser = argparse.ArgumentParser()
         
-    parser.add_argument('--mode', type=str, choices=['Import', 'TDV_Classification', 'TDV_Regression'],
-        help='Import - imports data to DB, TDV_Classification - splits data into test, dev & val sets for classification purpose, TDV_Regression - as previous for regression purpose '
-        , default='TDV_Classification')    
+    parser.add_argument('--mode', type=str, choices=['Import', 'TDV'],
+        help='Import - imports data to DB, TDV - splits data into test, dev & val'
+        , default='TDV')    
         
     parser.add_argument('--src_dir', type=str,
         help='Directory containing txt files to import'
